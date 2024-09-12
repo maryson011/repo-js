13265bb1004d22ad737690f147c9479a345ac2fc
maryson gleison <maryson011@gmail.com>
2024-09-12 12:18:15 -0300

    feat: array

---
+++ typeScript/initial/src/tipos/array.ts
@@ -0,0 +1,8 @@
+let hobbies: string[] = ['Cozinhar', 'Praticar Corrida']
+
+console.log(hobbies[0])
+console.log(typeof hobbies)
+
+let endereco: [string, number] = ['Rua A', 99]
+endereco = ['Rua B', 22]
+console.log(endereco)
