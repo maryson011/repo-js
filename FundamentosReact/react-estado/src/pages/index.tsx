a23a77a8a923f31d50f49fd5b6e4750d0e9b76bb
maryson gleison <maryson011@gmail.com>
2024-09-18 16:56:44 -0300

    feat: title (react-estates)

--- FundamentosReact/react-estado/src/pages/index.tsx
+++ FundamentosReact/react-estado/src/pages/index.tsx
@@ -4,10 +4,20 @@ import { IconForms } from "@tabler/icons-react";
 export default function Home() {
   return (
     <div className={`
-      flex flex-col justify-center items-center h-screen
+      flex flex-col justify-center items-center h-screen gap-20
       bg-gradient-to-br from-zinc-500 to-zinc-950
     `}>
-      <MenuItem icone={<IconForms />} texto="Estado" url="/revisao/estado"/>
+      <div className={`text-6xl`}>
+        <span className={`opacity-50 font-thin`}>Gerenciamento de </span>
+        <span className={`
+            font-black bg-gradient-to-r
+            from-blue-500 to-green-500
+            text-transparent bg-clip-text
+          `}>Estado</span>
+      </div>
+      <div>
+        <MenuItem icone={<IconForms />} texto="Estado" url="/revisao/estado"/>
+      </div>
     </div>
   );
 }
